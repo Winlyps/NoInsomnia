76ea88c5b95e0b5067f86c4ecb72967946bb3843
Winlypse <nts3@o2.pl>
2024-10-22 14:46:50 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noInsomnia/NoInsomnia.kt
@@ -0,0 +1,14 @@
+package winlyps.noInsomnia
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoInsomnia : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
